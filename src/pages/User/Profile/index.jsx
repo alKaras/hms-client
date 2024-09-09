99a47e69e33e59048dc9a3059a0a7f5d63cc0c73
alex_karas <al.karas.pr@gmail.com>
2024-09-09 23:52:02 +0300

    minor fixes with ui

--- src/pages/User/Profile/index.jsx
+++ src/pages/User/Profile/index.jsx
@@ -5,50 +5,60 @@ import {Button, Col, Row} from "react-bootstrap";
 import headerStyles from "../../../components/Header/Header.module.scss";
 import Form from "react-bootstrap/Form";
 import IntlTelInput from "intl-tel-input/reactWithUtils";
+import {useSelector} from "react-redux";
+import {infoAboutUser, selectIsLogged} from "../../../redux/slices/authSlice";
 
 export default function UserProfile() {
+    const user = useSelector(infoAboutUser);
+    const isLogged = useSelector(selectIsLogged);
+    const mainRole = isLogged && user.roles;
     return (
         <>
             <Header/>
-            <div className={UserProfileStyles.root}>
-                <Row className={UserProfileStyles.content}>
-                    <Col className={UserProfileStyles.leftSide} lg={9} md={9} xs={9}>
-                        <div className={UserProfileStyles.image}>
-                            <img src="/assets/profile.jpg" alt="profile"/>
-                        </div>
-                        <div className={UserProfileStyles.title}>Oleksandr Karas</div>
-                        <div className="d-flex flex-column">
-                            <label>Пошта</label>
-                            <input
-                                type="text"
-                                value={"al.karas.pr@gmail.com"}
-                                disabled={true}
-                                // className={`${errors.email ? RegisterStyles['error-input'] : ''}`}
-                            />
-                        </div>
-                        <div className="d-flex flex-column">
-                            <label>Телефон</label>
-                            <input type="text" value={"+380500000000"}/>
-                        </div>
-                        <div style={{marginTop: '10px'}} className="d-flex flex-column">
-                            <label>Новий пароль</label>
-                            <input
-                                type={"password"}
-                            />
-                        </div>
-                        <Button type={"submit"}>Зберегти</Button>
-                    </Col>
-                    <Col className={UserProfileStyles.rightSide} lg={3} md={3} xs={3}>
-                        <ul>
-                        {[...Array(3)].map((item, index) => (
-                            <li className={UserProfileStyles.roleBadge}>
-                                Role Name
-                            </li>
-                        ))}
-                        </ul>
-                    </Col>
-                </Row>
-            </div>
+            {isLogged ? (
+                <div className={UserProfileStyles.root}>
+                    <Row className={UserProfileStyles.content}>
+                        <Col className={UserProfileStyles.leftSide} lg={9} md={9} xs={9}>
+                            <div className={UserProfileStyles.image}>
+                                <img src="/assets/profile.jpg" alt="profile"/>
+                            </div>
+                            <div className={UserProfileStyles.title}>{user.data.name} {user.data.surname}</div>
+                            <div className="d-flex flex-column">
+                                <label>Пошта</label>
+                                <input
+                                    type="text"
+                                    value={user.data.email}
+                                    disabled={true}
+                                    // className={`${errors.email ? RegisterStyles['error-input'] : ''}`}
+                                />
+                            </div>
+                            <div className="d-flex flex-column">
+                                <label>Телефон</label>
+                                <input type="text" value={user.data.phone}/>
+                            </div>
+                            <div style={{marginTop: '10px'}} className="d-flex flex-column">
+                                <label>Новий пароль</label>
+                                <input
+                                    type={"password"}
+                                />
+                            </div>
+                            <Button type={"submit"}>Зберегти</Button>
+                        </Col>
+                        <Col className={UserProfileStyles.rightSide} lg={3} md={3} xs={3}>
+                            <ul>
+                                    <li className={UserProfileStyles.roleBadge}>
+                                        {mainRole}
+                                    </li>
+                            </ul>
+                        </Col>
+                    </Row>
+                </div>
+            ) : (
+                <>
+                    loading...
+                </>
+            )}
+
         </>
     );
 }
