bbbc79861272785402033e4686312464e8af4270
alex_karas <al.karas.pr@gmail.com>
2024-10-20 01:22:59 +0300

    refactor styles and variables on user services page

--- src/components/ServiceToggler/index.jsx
+++ src/components/ServiceToggler/index.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 import { useAccordionButton } from 'react-bootstrap';
 import togglerStyle from './ServiceToggler.module.scss';
 
-export const ServiceToggler = ({ children, eventKey, orderId, amountServices }) => {
+export const ServiceToggler = ({ children, eventKey, orderId, status, amountServices }) => {
     const decoratedOnClick = useAccordionButton(eventKey, () => (console.log('clicked')));
     return (
         <>
-            <div className={togglerStyle.root}>
+            <div className={`${togglerStyle.root}`} style={status === 'СКАСОВАНО' ? { color: 'gray' } : {}}>
                 <div className={togglerStyle.content}>
                     <div style={{ fontSize: '16px', fontWeight: 'bold' }}>Замовлення: {orderId}</div>
                     <div>К-сть послуг: {amountServices}</div>
                 </div>
-                <button className={togglerStyle['btn-toggler']} type="button"
-                    onClick={decoratedOnClick}>
-                    {children}
-                </button>
+                <div className={togglerStyle.content} style={{ marginBottom: 0 }}>
+                    <div>
+                        {status}
+                    </div>
+                    <button className={togglerStyle['btn-toggler']} type="button"
+                        onClick={decoratedOnClick}>
+                        {children}
+                    </button>
+                </div>
+
             </div>
         </>
     )
--- src/pages/User/Services/index.jsx
+++ src/pages/User/Services/index.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../../../components/Header'
 import UserServiceStyle from './Services.module.scss';
-import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getOrderByFilter } from '../../../api/httpApiClient';
 import { OrderFiltersEnum } from '../../../utils/enums/OrderFiltersEnum';
-import { Accordion, Card } from 'react-bootstrap';
+import { Accordion, Button, Card } from 'react-bootstrap';
 import { ServiceToggler } from '../../../components/ServiceToggler';
 import Moment from 'react-moment';
 
 export const UserServices = () => {
     const [isLoaded, setIsLoaded] = useState(false);
     const [userServiceData, setUserServiceData] = useState([]);
-
     const { _id } = useParams();
 
     useEffect(() => {
@@ -22,7 +20,9 @@ export const UserServices = () => {
         })
             .then((resp) => {
                 setIsLoaded(true);
-                setUserServiceData(resp.data.data);
+                //Output only sold orders
+                let tempUserData = resp.data.data.filter((item) => item.order.status === 2);
+                setUserServiceData(tempUserData);
             })
             .catch((err) => {
                 console.error(err.response);
@@ -34,32 +34,52 @@ export const UserServices = () => {
             <Header />
             <div className={UserServiceStyle.root}>
                 <h2 style={{ fontSize: '24px', marginBottom: '25px' }}>Мої послуги</h2>
-
-                {isLoaded && userServiceData.length > 0 ? userServiceData.map((item) => (
-                    <Accordion>
-                        <Card>
-                            <Card.Header>
-                                <ServiceToggler orderId={item.order.id} amountServices={item.services.length} eventKey="0">Послуги</ServiceToggler>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="0">
-                                <Card.Body>
-                                    {item.services.map((serviceData) => (
-                                        <li className={`d-flex justify-content-between align-items-center ${serviceData.is_canceled === 1 ? UserServiceStyle.canceled : ''}`} style={{ marginBottom: '15px', border: '1px solid gray', borderRadius: '15px', padding: '10px' }}>
-                                            <div>
-                                                <div style={{ marginBottom: '10px' }}>[{serviceData.timeslot.service.id}] {serviceData.timeslot.service.name}</div>
-                                                <div>Відділення: {serviceData.timeslot.department.title}</div>
-                                            </div>
-                                            <div>
-                                                <div style={{ marginBottom: '10px' }}>Дата початку: <Moment format='DD.MM.YYYY HH:mm:ss'>{serviceData.timeslot.start_time}</Moment></div>
-                                                <div>Ціна: {serviceData.timeslot.price}</div>
-                                            </div>
-                                        </li>
-                                    ))}
-                                </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                    </Accordion>
-                )) : (<></>)}
+                <div className={UserServiceStyle.content}>
+                    {isLoaded && userServiceData.length > 0 ? userServiceData.map((item) => (
+                        <Accordion>
+                            <Card>
+                                <Card.Header>
+                                    <ServiceToggler
+                                        orderId={item.order.id}
+                                        status={item.order.status === 2 ? 'ОПЛАЧЕНО' : (item.order.status === 3 ? "СКАСОВАНО" : "ОЧІКУВАННЯ ОПЛАТИ")}
+                                        amountServices={item.services.length}
+                                        eventKey="0"
+                                    >
+                                        <i class="fa-solid fa-angle-down"></i>
+                                    </ServiceToggler>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="0">
+                                    <Card.Body className={UserServiceStyle.cardsContent}>
+                                        {item.services.map((serviceData) => (
+                                            <li
+                                                className={`d-flex justify-content-between align-items-center 
+                                                    ${serviceData.is_canceled === 1 ? UserServiceStyle.canceled : ''} 
+                                                    ${UserServiceStyle.cardService}`
+                                                }
+                                            >
+                                                <div>
+                                                    <div style={{ marginBottom: '10px' }}>[{serviceData.timeslot.service.id}] {serviceData.timeslot.service.name}</div>
+                                                    <div>Відділення: {serviceData.timeslot.department.title}</div>
+                                                </div>
+                                                <div className={`d-flex justify-content-between align-items-center`}>
+                                                    <div>
+                                                        <div style={{ marginBottom: '10px' }}>Дата початку: <Moment format='DD.MM.YYYY HH:mm:ss'>{serviceData.timeslot.start_time}</Moment></div>
+                                                        <div>Ціна: {serviceData.timeslot.price}</div>
+                                                    </div>
+                                                    {serviceData.is_canceled === 0 ? (
+                                                        <Button className='btn btn-secondary' style={{ color: 'white', marginLeft: '15px' }}><i class="fa-solid fa-download"></i></Button>
+                                                    ) : (
+                                                        <></>
+                                                    )}
+                                                </div>
+                                            </li>
+                                        ))}
+                                    </Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>
+                    )) : (<></>)}
+                </div>
 
 
             </div>
